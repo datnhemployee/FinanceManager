e2a283887b941b10968ad85ed5f59a1b967c15e2
Fijetso <henrynguyen02081997@gmail.com>
2019-05-02 12:05:05 +0700

    Fix merge

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Detail from "./src/view/screen/Detail/Detail";
-
-import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
 // import FirstLetterIcon from './src/view/component/FirstLetterIcon/FirstLetterIcon';
-import Home from './src/view/screen/Home/Home';
-import SpenseController from './src/controller/SpenseController';
+import Home from "./src/view/screen/Home/Home";
+import SpenseController from "./src/controller/SpenseController";
 
-
-const url = 
-  `https://facebook.github.io/react-native/docs/assets/favicon.png`;
+const url = `https://facebook.github.io/react-native/docs/assets/favicon.png`;
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
-  async componentDidMount() {}
-
-    let test = 
-    SpenseController.insert_spense()
+  async componentDidMount() {
+    let test = SpenseController.insert_spense();
   }
 
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         {/* <Home /> */}
         {/* <FirstLetterIcon 
           // firstLetter = {}
